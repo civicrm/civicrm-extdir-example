f3c94af3cf6fbb69cf62c64906c0639080875eb9
Tim Otten <totten@civicrm.org>
2017-09-12 22:54:32 -0700

    extdir.sh, single - Preserve physical location of XML files
    The change from 68a88792062ad66599fe878681b8c8b9b417509c works on newer
    versions of Civi but (probably) breaks compatibility with older versions.
    
    This should work with both.

--- single/index.php
+++ single/index.php
@@ -1,8 +1,8 @@
 <?php
 
 $extensions = array();
-foreach (glob('*.xml') as $fileName) {
-  $extName = str_replace('.xml', '', $fileName);
+foreach (glob(__DIR__.'/../*.xml') as $fileName) {
+  $extName = str_replace('.xml', '', basename($fileName));
   $extensions[$extName] = file_get_contents($fileName);
 }
 echo json_encode($extensions);
